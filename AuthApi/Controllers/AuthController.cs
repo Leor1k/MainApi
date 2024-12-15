8299da6be34a032e9ffd0018193f0a77a417294a
Leorik <acc.8uy@yandex.ru>
2024-12-15 18:46:55 +0300

    Initial commit

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using AuthApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest loginRequest)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.username == loginRequest.username && u.password == loginRequest.password);
+
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Invalid credentials" });
+            }
+            return Ok(new { message = "Login successful" });
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _context.Users.ToListAsync(); // Получаем всех пользователей из базы данных
+
+            if (users == null || users.Count == 0)
+            {
+                return NotFound(new { message = "No users found" }); // Если пользователей нет
+            }
+
+            return Ok(users); // Возвращаем список пользователей
+        }
+
+    }
+}
---
+++ AuthApi/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuthApi.Models;
+
+namespace AuthApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Указываем имя таблицы с маленькими буквами
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
