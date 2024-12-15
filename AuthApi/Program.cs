00c8ab3e652442fe923df1e439125bb0e69cca4a
Leorik <acc.8uy@yandex.ru>
2024-12-15 19:40:21 +0300

    Поправил слушание

--- AuthApi/Program.cs
+++ AuthApi/Program.cs
@@ -1,10 +1,10 @@
 ﻿using AuthApi.Data;
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Добавляем сервисы
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 builder.Services.AddEndpointsApiExplorer();
@@ -12,16 +12,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Устанавливаем сервер на 0.0.0.0:5000
+app.Urls.Add("http://0.0.0.0:5000");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// Маршруты
 app.MapControllers();
 
 app.Run();
