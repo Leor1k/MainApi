ebe4a5b7b9ffe4512e8015963aa610414e87bab3
Leorik <acc.8uy@yandex.ru>
2024-12-16 16:53:14 +0300

    так, я ебал рты этих блять комбинаций

--- AuthApi/Models/User.cs
+++ AuthApi/Models/User.cs
@@ -1,7 +1,10 @@
-﻿namespace AuthApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthApi.Models
 {
     public class User
     {
+        [Key]  // Это явно указывает, что user_id - это первичный ключ
         public int user_id { get; set; }
         public string users_name { get; set; }
         public string users_email { get; set; }
