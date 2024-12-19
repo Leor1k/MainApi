e1c8d9ba17aff7486b777189dc0f9d9e2144e8da
Leorik <acc.8uy@yandex.ru>
2024-12-19 17:08:55 +0300

    Старый роет его ебать добрый нижний регистр

--- AuthApi/Models/User.cs
+++ AuthApi/Models/User.cs
@@ -9,9 +9,9 @@ namespace AuthApi.Models
         public string users_name { get; set; }
         public string users_email { get; set; }
         public string users_password { get; set; }
-        public bool isConfirmed { get; set; } = false;
-        public string? confirmationCode { get; set; }
-        public DateTime? createdAt { get; set; } = DateTime.UtcNow;
+        public bool isconfirmed { get; set; } = false;
+        public string? confirmationcode { get; set; }
+        public DateTime? createdat { get; set; } = DateTime.UtcNow;
 
     }
 }
