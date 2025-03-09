6184bb131fc046f926017eeb7ca3396d947f775b
Leorik <acc.8uy@yandex.ru>
2025-03-09 16:52:40 +0300

    Гошка куценков

--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7c193836dac6dbba9cb7c5ed18e8c330a3bb24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afafd19c52e354f173320812d289427b9b94d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
