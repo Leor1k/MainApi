235fbc3e39f1f57d610bafa44b024ea836f28698
Leorik <acc.8uy@yandex.ru>
2025-03-15 15:27:05 +0300

    ничего не понимаю

--- AuthApi/Models/Requests/FriendSignalR.cs
+++ AuthApi/Models/Requests/FriendSignalR.cs
@@ -2,10 +2,10 @@
 {
     public class FriendSignalR
     {
-        int FriendId { get; set; }
-        int UserId { get; set; }
-        string? UserName { get; set; }
-        string? PhotoName { get; set; }
+        public int FriendId { get; set; }
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? PhotoName { get; set; }
         public FriendSignalR(int friendId, int userId, string? userName, string? photoName)
         {
             FriendId = friendId;
--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a667e71e11d4abe400ec2697c56d1e93bfed8a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2356b05fcdd3a964b796faea2ae0eb500f85c800")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
